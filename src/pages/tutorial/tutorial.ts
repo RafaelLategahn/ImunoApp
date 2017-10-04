6abf25b8279b89742699e6c1b61fae712c105bc4
Rafael Lategahn <rafael@lategahn.eng.br>
2017-10-04 19:37:16 -0300

    first commit imunoapp

---
+++ src/pages/tutorial/tutorial.ts
@@ -0,0 +1,74 @@
+import { Component } from '@angular/core';
+import { IonicPage, MenuController, NavController, Platform } from 'ionic-angular';
+
+import { TranslateService } from '@ngx-translate/core';
+
+export interface Slide {
+  title: string;
+  description: string;
+  image: string;
+}
+
+@IonicPage()
+@Component({
+  selector: 'page-tutorial',
+  templateUrl: 'tutorial.html'
+})
+export class TutorialPage {
+  slides: Slide[];
+  showSkip = true;
+  dir: string = 'ltr';
+
+  constructor(public navCtrl: NavController, public menu: MenuController, translate: TranslateService, public platform: Platform) {
+    this.dir = platform.dir();
+    translate.get(["TUTORIAL_SLIDE1_TITLE",
+      "TUTORIAL_SLIDE1_DESCRIPTION",
+      "TUTORIAL_SLIDE2_TITLE",
+      "TUTORIAL_SLIDE2_DESCRIPTION",
+      "TUTORIAL_SLIDE3_TITLE",
+      "TUTORIAL_SLIDE3_DESCRIPTION",
+    ]).subscribe(
+      (values) => {
+        console.log('Loaded values', values);
+        this.slides = [
+          {
+            title: values.TUTORIAL_SLIDE1_TITLE,
+            description: values.TUTORIAL_SLIDE1_DESCRIPTION,
+            image: 'assets/img/cebeb675f978f378aa92d7b781f31398.png',
+          },
+          {
+            title: values.TUTORIAL_SLIDE2_TITLE,
+            description: values.TUTORIAL_SLIDE2_DESCRIPTION,
+            image: 'assets/img/logo_cores_pqn.png',
+          },
+          {
+            title: values.TUTORIAL_SLIDE3_TITLE,
+            description: values.TUTORIAL_SLIDE3_DESCRIPTION,
+            image: 'assets/img/dr_prescri.png',
+          }
+        ];
+      });
+  }
+
+  startApp() {
+    this.navCtrl.setRoot('WelcomePage', {}, {
+      animate: true,
+      direction: 'forward'
+    });
+  }
+
+  onSlideChangeStart(slider) {
+    this.showSkip = !slider.isEnd();
+  }
+
+  ionViewDidEnter() {
+    // the root left menu should be disabled on the tutorial page
+    this.menu.enable(false);
+  }
+
+  ionViewWillLeave() {
+    // enable the root left menu when leaving the tutorial page
+    this.menu.enable(true);
+  }
+
+}
